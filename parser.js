00ff38aefcba12dd0f549278ad2444b324b78202
dorfir <greendorfir@gmail.com>
2026-01-09 11:07:23 +0100

    v1.4

--- parser.js
+++ parser.js
@@ -26,16 +26,26 @@ const eventEmitter = new EventEmitter();
 
 
 function launchSetOfDownloads(url_set) {
+
+  let valid_downloads = []
+
   eventEmitter.on('download_complete', (index) => {
-    console.log('download_complete event recieved, n°' + index)
+    // console.log('download_complete event received, n°' + index)
+    valid_downloads[index] = true
+    let is_all_valid = true
+    valid_downloads.forEach(entry => {
+      is_all_valid = (is_all_valid && entry)
+    })
+    if (is_all_valid) eventEmitter.emit('set_of_downloads_complete')
   })
 
   url_set.forEach((url_entry, url_index) => {
+    valid_downloads.push(false)
     download(url_prefix_distant + url_entry.url, url_prefix_local + url_entry.url.split('/').splice(-1), function(err) {
       if (err) {
         console.log('Download '+ url_index +' failed: ' + err)
       } else {
-        console.log('Download '+ url_index +' succeeded');
+        // console.log('Download '+ url_index +' succeeded')
         eventEmitter.emit('download_complete', url_index)
       }
     })
@@ -44,20 +54,38 @@ function launchSetOfDownloads(url_set) {
 
 
 function chunkArray(arr, size) {
-  const res = [];
-  for (let i = 0; i < arr.length; i += size) res.push(arr.slice(i, i + size));
-  return res;
+  const res = []
+  for (let i = 0; i < arr.length; i += size) res.push(arr.slice(i, i + size))
+  return res
 }
+
+
 
 try {
   let clean_list = iterate('export.json')
   let split_list = chunkArray(clean_list, 5)
-  console.log(split_list[0])
-  launchSetOfDownloads(split_list[0])
+
+  let index_chunk = 62
+  console.log(split_list.length)
+  console.log(split_list[62])
+  launchSetOfDownloads(split_list[index_chunk])
+  eventEmitter.on('set_of_downloads_complete', () => {
+    console.log(`-- set_of_downloads_complete - ${index_chunk}`)
+    index_chunk++
+    if (index_chunk < split_list.length) {
+      setTimeout(() => {
+        launchSetOfDownloads(split_list[index_chunk])
+      }, 1000)
+    }
+  })
+
 } catch(err) {
   console.log(err)
 
 }
+
+
+/* Divers */
 
 
 // try {
